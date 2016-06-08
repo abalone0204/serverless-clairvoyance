17c205f9fa463be1a77aeb5b242ad797a2318c8b
abalone0204 <abalone0204@gmail.com>
2016-06-09 02:40:41 +0800

    record fb_id

--- resources/comments/handler.js
+++ resources/comments/handler.js
@@ -45,11 +45,11 @@ module.exports.handler = (event, context) => {
                             console.log('userData: ', userData);
                             if (event.anonymous === 'false') {
                                 event.payload.Item.user_name = userData.Items[0].user_name
-                                event.payload.Item.fb_id = userData.Items[0].fb_id
                                 event.payload.Item.anonymous = false
                             } else {
                                 event.payload.Item.anonymous = true
                             }
+                            event.payload.Item.fb_id = userData.Items[0].fb_id
                             event.payload.Item.type = event.type
                             event.payload.Item.timestamp = Date.now()
                             dynamo.putItem(event.payload, (err, data) => {
